cff434ce68672f04b1c39608cf17c99404f8ca41
Hoàng Lê Minh <leminhdragon@gmail.com>
2022-03-12 22:58:13 +0700

    Hoàn chỉnh

--- REGISTRATION-master/src/main/resources/static/js/statisticAction.js
+++ REGISTRATION-master/src/main/resources/static/js/statisticAction.js
@@ -36,20 +36,43 @@ function billsByMonthAndYear(month, year) {
 		url: url
 	}).done(function(result) {
 		$.each(result, function(i, bill) {
-			$("#tbody").append("<tr><th><a href='' class='link-detail' rowNumber='"+bill.id+"'>Watch bill details</a>"+
-			"<div class='modal' id='billDetailModal"+bill.id+"' style='width: 900px; margin: auto; padding: 0 !important; left: 50%; top: 50%; transform: translate(-50%, -50%); position: absolute;'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'></div></div></div></div>"+
-			"</th><td>"+bill.toTal+"</td><td>"+bill.howToPay+"</td><td>"+bill.buyDate+"</td><td>"+bill.user.phoneNumber+"</td><td>"+bill.user.fullName+"</td><td>"+bill.address+"</td><td>"+bill.status.statusName+"</td></tr>");
+			$("#tbody").append("<tr><th><a href='' class='link-detail' rowNumber='" + bill.id + "'>Watch bill details</a>" +
+				"<div class='modal' id='billDetailModal" + bill.id + "' style='width: 900px; margin: auto; padding: 0 !important; left: 50%; top: 50%; transform: translate(-50%, -50%); position: absolute; height: 700px; margin: auto;'><div class='modal-dialog'><div class='modal-content'>" +
+				"<div class='modal-header'><h4 class='modal-title'>LIST BILL DETAILS</h4>" +
+				"<div class='modal-body' style='width: 700px; height: 700px; margin: auto;'>" +
+				"<table id='billDetailTable' style = 'width: 100%; margin: auto;'>" +
+				"<thead class='thead-dark'>"+
+				"<tr>"+
+				"<th scope='col'>IMAGE</th>"+
+				"<th scope='col'>NAME</th>"+
+				"<th scope='col'>PRICE</th>"+
+				"<th scope='col'>QUANTITY</th>"+
+				"<th scope='col'>MONEY</th>"+
+				"</tr></thead>"+
+				"<tbody></tbody>"+
+				"</table></div></div></div></div></div>" +
+				"</th><td>" + bill.toTal + "</td><td>" + bill.howToPay + "</td><td>" + bill.buyDate + "</td><td>" + bill.user.phoneNumber + "</td><td>" + bill.user.fullName + "</td><td>" + bill.address + "</td><td>" + bill.status.statusName + "</td></tr>");
 		});
 	});
-	
+
 	$(document).on("click", ".link-detail", function(evt) {
 		evt.preventDefault();
-		billDetail($(this));
+		getListBillDetail($(this));
 	});
 }
 
-function billDetail(link){
+function getListBillDetail(link) {
 	rowNumber = link.attr("rowNumber");
-	modal = document.getElementById("billDetailModal"+rowNumber);
+	$("#billDetailTable tbody").empty();
+	$.ajax({
+		type: "GET",
+		url: contextPath + "listBillDetail/" + rowNumber
+	}).done(function(result) {
+		$.each(result, function(index, billDetail) {
+			var row = "<tr><th><img src='data:image/jpeg;base64,"+billDetail.product.image+"' style='width: 100px; height: 100px;'></th><td>" + billDetail.product.name + "</td><td>" + billDetail.productPrice + "</td><td>" + billDetail.quantity + "</td><td>" + billDetail.money + "</td></tr>";
+			$("#billDetailTable tbody").append(row);
+		});
+	});
+	modal = document.getElementById("billDetailModal" + rowNumber);
 	modal.style.display = "block";
 }
